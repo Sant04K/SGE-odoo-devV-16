964d2d7e71c4c3d0cf366f3dcff937e162b2fc68
Santos Valdivielso <vlady.valdy@gmail.com>
2026-01-22 12:29:22 +0100

    Añadida Vista Kanban, y muchos widgets graciosos, correción de pequeños errores en las vistas.

--- odoo/addons/svl_agenciaviajes/__manifest__.py
+++ odoo/addons/svl_agenciaviajes/__manifest__.py
@@ -28,11 +28,12 @@ Gestiona una agencia de viajes, permitiendo llevar el control de los clientes, v
         
 
     #Importante que el "menus.xml" esté antes que las vistas para que se creen los menús correctamente
-        'views/svl_agenciaviajes_menu.xml',
         'views/svl_agenciaviajes_viaje.xml',
-        'views/svl_agenciaviajes_cliente.xml',
         'views/svl_agenciaviajes_destino.xml',
         'views/svl_agenciaviajes_temporada.xml',
+        'views/svl_agenciaviajes_viaje_kanban.xml',
+        'views/svl_agenciaviajes_menu.xml',
+        'views/svl_agenciaviajes_cliente.xml',
 
     #Informes
         'reports/report_cliente.xml',
